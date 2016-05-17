43658f79465b5a06d9e50fe132e7ff0d43fa7f5a
Kendra Lyndon <kendralyndon@gmail.com>
2016-05-17 10:31:32 -0600

    add all Bob responses

--- bob.js
+++ bob.js
@@ -6,8 +6,12 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-  if(input === input.toUpperCase()){
+  if((input.match(/[a-z]/i)) && (input === input.toUpperCase())){
     return 'Whoa, chill out!';
+  }else if(input.charAt(input.length -1) === '?'){
+    return 'Sure.';
+  }else if((/\S/.test(input)) === false){
+    return 'Fine. Be that way!';
   }else{
     return 'Whatever.';
   }
